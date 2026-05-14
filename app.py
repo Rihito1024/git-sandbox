8254a6e5abb3b4277ab6a2e884761f1570d58933
exture-fukuda <fukuda@ex-ture.jp>
2026-05-14 18:19:47 +0900

    app.pyの中の縦積みになっているものを横並びになるように変更しました。

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ st.title("月別売上ダッシュボード")
 
 df = pd.read_csv("data/sales.csv", index_col="月")
 
-st.header("売上推移グラフ")
-st.line_chart(df)
-
-st.header("売上データ一覧")
-st.dataframe(df, use_container_width=True)
+col1, col2 = st.columns(2)
+with col1:
+    st.header("売上推移")
+    st.line_chart(df)
+with col2:
+    st.header("データ一覧")
+    st.dataframe(df)
 
 st.header("月別合計")
 st.bar_chart(df.sum(axis=1))
